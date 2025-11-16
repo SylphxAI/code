d9c43780ccb5dfa5ea5109a8460d2e0b5bc76288
Kyle Tse <shtse8@gmail.com>
2025-11-16 21:53:28 +0000

    fix: correct zen-react import in useQueuedMessages hook

--- packages/code-client/src/hooks/useQueuedMessages.ts
+++ packages/code-client/src/hooks/useQueuedMessages.ts
@@ -2,13 +2,13 @@
  * Hook to access queued messages for current session
  */
 
-import { useSignal } from "@sylphx/zen/react";
+import { useStore } from "@sylphx/zen-react";
 import { $currentSessionId } from "../signals/domain/session/index.js";
 import { $sessionQueues } from "../signals/domain/queue/index.js";
 
 export function useQueuedMessages() {
-	const currentSessionId = useSignal($currentSessionId);
-	const sessionQueues = useSignal($sessionQueues);
+	const currentSessionId = useStore($currentSessionId);
+	const sessionQueues = useStore($sessionQueues);
 
 	const queuedMessages = currentSessionId ? sessionQueues[currentSessionId] || [] : [];
 
